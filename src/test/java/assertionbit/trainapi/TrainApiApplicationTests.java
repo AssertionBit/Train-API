68ea268af96ecd99f9f2eda3bca8ce6c441d4b8f
AssertionBit <markushasorokin@yandex.com>
2024-02-01 17:12:19 +0300

    Initial commit

---
+++ src/test/java/assertionbit/trainapi/TrainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package assertionbit.trainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
